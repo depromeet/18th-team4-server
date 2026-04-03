b9f2d907b3333665b710231aca53db6ac6acc618
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2026-04-03 22:39:15 +0900

    init project

---
+++ src/test/java/com/readwith/ReadwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
