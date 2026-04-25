c2fc39338344c0a9e46c82707c59b303d7deab51
psychology50 <qud1251@gmail.com>
2026-04-26 03:06:12 +0900

    docs: swagger dev server domain 추가

--- src/main/java/com/readum/presentation/common/swagger/OpenApiConfig.java
+++ src/main/java/com/readum/presentation/common/swagger/OpenApiConfig.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @Configuration
 @Profile("dev")
@@ -22,6 +25,10 @@ public class OpenApiConfig {
                         .title("Readum API")
                         .description("Readum 서비스 API 문서")
                         .version("v1"))
+                .servers(List.of(
+                        new Server().url("https://api.readum.kr").description("Dev Server"),
+                        new Server().url("http://localhost:8080").description("Local")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
